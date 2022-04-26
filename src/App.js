76e429267eb184950a96cdd47d3fd612e7539ab0
subbareddygithub <Annapureddy@123>
2022-04-26 12:58:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import JobInformation from "./components/JobInformation";
+import PersonalInformationComponent from "./components/PersonalInformationComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderComponent />
+      <div>
+        <JobInformation />
+      </div>
+      <div>
+        <PersonalInformationComponent />
+      </div>
+      <div>
+        <br />
+      </div>
+      
+      <div>
+        <FooterComponent />
+      </div>
     </div>
   );
 }
